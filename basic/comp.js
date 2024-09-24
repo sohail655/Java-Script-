4a7883f7bfb2f53c1c867340be9831f96fddc721
Sohail Ahmad <181189626+sohail655@users.noreply.github.com>
2024-09-24 07:51:25 +0000

    comparision

---
+++ basic/comp.js
@@ -0,0 +1,22 @@
+// console.log(2 == 2);
+// console.log(2 != 2);
+// console.log(2 < 2);
+// console.log(2 > 2);
+// console.log(2 >= 2);
+// console.log(2 <= 2);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+// === check strictly and always condation
+
+console.log("2" === 2);
+
