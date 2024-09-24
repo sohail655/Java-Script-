c9ce438a7aec0521e69405ed945891651e525f78
Sohail Ahmad <181189626+sohail655@users.noreply.github.com>
2024-09-24 06:14:37 +0000

    v.js

---
+++ basic/dt.js
@@ -0,0 +1,30 @@
+"use strict"; // Treat all code as newer version
+
+// alert(4+4); // We are using node.js not browser
+
+console.log(3
+    +
+    3);// always prefer code reability
+
+console.log("Sohail");
+
+let name = "Sohail"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//strings
+//number
+//bigInt
+//bolean
+//null
+//undefined
+//symbol
+
+//Objects 
+
+console.log(typeof undefined);  // undrfined
+console.log(typeof null); // objects
+
+
+
