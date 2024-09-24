1614b9117d7dd7e4db3ce2cb02184da60b5fd6ec
Sohail Ahmad <181189626+sohail655@users.noreply.github.com>
2024-09-24 05:37:55 +0000

    v.js

---
+++ basic/v.js
@@ -0,0 +1,14 @@
+const id = 123456
+let email = "sohail@gmail.com"
+// var password = "434343" // never use var key word
+userCity = "jampur"
+
+// id = "7594695" // can not cahange the value of const
+
+email = "d@gogle.com"
+password = "985900"
+userCity= "rajanpur"
+
+console.log(email);
+
+console.table([email,id,password,userCity])
